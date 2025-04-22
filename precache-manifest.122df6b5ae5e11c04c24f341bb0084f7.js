98e0ed9b70e838eaa5bfda7dc50b1f94542cb358
Vishnu Simha <vishnusimha01@gmail.com>
2025-04-22 14:10:42 +0100

    Updates

---
+++ precache-manifest.122df6b5ae5e11c04c24f341bb0084f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2b2c04cdf7cdeabffd16bc597a4b01",
+    "url": "/react-weather-dashboard/index.html"
+  },
+  {
+    "revision": "7ef1ce2a9a6763867678",
+    "url": "/react-weather-dashboard/static/css/main.1f195d06.chunk.css"
+  },
+  {
+    "revision": "da8b8ee81106c8324f14",
+    "url": "/react-weather-dashboard/static/js/2.4519393a.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/react-weather-dashboard/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "7ef1ce2a9a6763867678",
+    "url": "/react-weather-dashboard/static/js/main.68aa2740.chunk.js"
+  },
+  {
+    "revision": "32da81bc72133322ffbb",
+    "url": "/react-weather-dashboard/static/js/runtime~main.b2258d89.js"
+  }
+]);
